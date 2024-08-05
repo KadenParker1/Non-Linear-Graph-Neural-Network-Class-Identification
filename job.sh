1c6cb2b4f3cf26ee61004c69c30bf8bfe175ff97
Kaden Parker <bobert11@login04.rc.byu.edu>
2024-08-05 10:38:48 -0600

    Added job.sh file to run slurm jobs on the super computer

---
+++ job.sh
@@ -0,0 +1,15 @@
+#SBATCH --time=01:00:00   # walltime
+##SBATCH --acount=neo
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=32G   # memory per CPU core
+#SBATCH --gpus=1
+#SBATCH -J "nonlineargnnst"   # job name
+
+
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+source /apps/miniconda3/latest/etc/profile.d/conda.sh
+conda activate boydresearch
+
+srun -u python3 polarwauto.py 
